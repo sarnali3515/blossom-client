f769ec952aa657d69fcb112a34a6dde03abd440f
sarnali15 <khatuna15-3515@diu.edu.bd>
2024-08-16 15:53:44 +0600

    basic set up done

---
+++ src/layout/Main.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+
+const Main = () => {
+    return (
+        <div>
+            abc
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Main;
